8864e467a98ad0223bfea3f454e894928547e2f2
Brandon Patterson <brandnpatterson@gmail.com>
2017-10-19 15:01:58 -0500

    changed name

---
+++ people.php
@@ -0,0 +1,13 @@
+<?php
+
+$brandon = new Person('Brandon', 29);
+$lauren = new Person('Lauren', 24);
+$matthew = new Person('Matthew', 21);
+$justin = new Person('Justin', 22);
+
+$people = array(
+  $brandon,
+  $lauren,
+  $matthew,
+  $justin
+);
